b301750eed36bdd879f2fad946927ef7b60c3292
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-10-10 13:03:54 +0700

    clean up main

--- src/utils/helperFunc.ts
+++
@@ -1,4 +0,0 @@
-export function shortenAddress(address = '', chars = 4): string {
-  if (chars >= address.length) return address;
-  return `${address.slice(0, chars)}...${address.slice(-chars)}`;
-}
