8126d5a37ddcf9479c23c7d187a5412c5c858126
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-10-12 11:52:52 +0700

    update wallets

--- example/sol/SolExample.tsx
+++ example/sol/SolExample.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useMemo, useState } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-ant-design';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { getPhantomWallet } from '@solana/wallet-adapter-wallets';
+import { getPhantomWallet, getSolflareWallet } from '@solana/wallet-adapter-wallets';
 import { web3 } from '@project-serum/anchor';
 import { SolMarketplaceExample } from './SolMarketplaceExample';
 import { CandyShopDataValidator } from '../../core/ui/.';
@@ -10,32 +10,11 @@ import { CandyShop } from '../../core/sdk/.';
 import { ShopConfig } from './ShopConfig';
 import { Cluster } from '@solana/web3.js';
 
-const activeStyle = { pointerEvent: 'none', paddingRight: 20, color: 'black', fontWeight: 'bold' };
-const normalStyle = { paddingRight: 20 };
-
-enum PageRoute {
-  MarketPlace = '/',
-  Auction = '/auction',
-  EditionDrop = '/edition-drop'
-}
-
-const initiateRoutePage = () => {
-  switch (window.location.pathname) {
-    case PageRoute.Auction:
-      return PageRoute.Auction;
-    case PageRoute.EditionDrop:
-      return PageRoute.EditionDrop;
-    default:
-      return PageRoute.MarketPlace;
-  }
-};
-
 export const SolExample: React.FC = () => {
-  const [pageRoute, setPageRoute] = useState<PageRoute>(initiateRoutePage());
   const [candyShop, setCandyShop] = useState<CandyShop | null>();
 
   const endpoint = useMemo(() => web3.clusterApiUrl((candyShop?.env || 'devnet') as Cluster), [candyShop?.env]);
-  const wallets = useMemo(() => [getPhantomWallet()], []);
+  const wallets = useMemo(() => [getPhantomWallet(), getSolflareWallet()], []);
 
   const onSetCandyShop = useCallback((candyShop: CandyShop) => setCandyShop(candyShop), []);
 
@@ -55,18 +34,8 @@ export const SolExample: React.FC = () => {
               >
                 <div>
                   <span style={{ fontWeight: 'bold', fontSize: 16, marginRight: 20 }}>Carbon Market</span>
-                  {/* <Link
-                    style={pageRoute === PageRoute.MarketPlace ? activeStyle : normalStyle}
-                    to={PageRoute.MarketPlace}
-                    onClick={() => setPageRoute(PageRoute.MarketPlace)}
-                  >
-                    Marketplace
-                  </Link> */}
                 </div>
                 <div>
-                  {/* <Link style={activeStyle} to="/">
-                    SOL
-                  </Link> */}
                   <ShopConfig onSetCandyShop={onSetCandyShop} />
                   <WalletMultiButton />
                 </div>
